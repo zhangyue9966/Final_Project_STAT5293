3ccad121eef305e59511e80266fc3b30dfc2eaed
Kaiyan Zheng <kz2324@columbia.edu>
2019-04-30 14:40:44 -0400

    Add files via upload

---
+++ scrape_yelp/scrape_yelp_western.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import pandas as pd
+
+API_KEY = '9zugF8ss_jbQwpd-Ms-NtO2oarzoDG6UDyx7cZqEitL-jZnX3YS5XaIogtgRGOezaezIv-eQTaf6O0a538NZdG-FQL710MZ6NAs2jAUXE7KqlAII_mGLlifMgsCiXHYx'
+URL = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+def search_yelp(terms):
+    business_data_western = []
+
+    # Yelp API allows for max limit of 50 results and max 1000 results total using offset
+    for term in terms:
+        cuisine = term.split(' ')[0]
+        for offset in range(0, 1000, 50):
+            parameters = {
+                'term': term,
+                'limit': 50,
+                'location': 'New York',
+                'offset': offset
+                }
+
+            # Make a request to the Yelp API
+            response = requests.get(URL, headers=HEADERS, params=parameters)
+            if response.status_code == 200:
+                results = response.json()['businesses']
+                for item in results:
+                    # Add cuisine to resulting
+                    item['cuisine'] = cuisine
+                business_data_western.extend(results)   
+            elif response.status_code == 400:
+                print('400 Bad Request')
+                break
+    return business_data_western
+
+def output_total_data(df):
+    df.to_csv('business_data_western.csv', index=False)
+    data = df.to_json(orient='records')
+    with open('business_data_western.json', 'w') as outfile:
+        json.dump(json.loads(data), outfile, indent=4)
+
+def main():
+    categories = ['italian', 'spanish', 'greek','french', 'mexican']
+    # Added 'food' to query options 
+    terms = ['restaurants', 'food']
+    queries = ['%s %s' % (cat, term) for term in terms for cat in categories]
+    business_data_western = search_yelp(queries)
+
+    df = pd.DataFrame(business_data_western)
+    df.drop_duplicates(['id'], keep='first', inplace=True)
+
+    output_total_data(df)
+
+if __name__ == '__main__':
+    main()
